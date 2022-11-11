4e2382993c16a4f727e88ccac4a475d98e285aa6
sserve-kr <personal@sserve.me>
2022-11-12 07:40:14 +0900

    Fixed urls.py error

--- wompweb/urls.py
+++ wompweb/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from django.views.generic impor TemplateView
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', include('main.urls', namespace='main')),

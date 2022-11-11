0ea664221dd6bc925ddc7ff4f31b46a770296517
sserve-kr <personal@sserve.me>
2022-11-12 07:38:17 +0900

    Added robots.txt and sitemap.xml for search engine

--- wompweb/urls.py
+++ wompweb/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from django.views.generic impor TemplateView
 
 urlpatterns = [
     path('', include('main.urls', namespace='main')),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='text/xml')),
 ]

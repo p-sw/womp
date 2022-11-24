624bbca573b9a2ffb12d9a010f41ca31f9e13f1a
sserve-kr <personal@sserve.me>
2022-11-24 09:24:04 +0000

    Changed db location

--- wompweb/settings/base.py
+++ wompweb/settings/base.py
@@ -37,7 +37,7 @@ WSGI_APPLICATION = 'wompweb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db', 'db.sqlite3'),
     }
 }
 
